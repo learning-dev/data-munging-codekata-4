00a6c101ee259753dc5e94f7d73986a89e0ad702
learning-dev <ayazm.140895@gmail.com>
2019-12-06 11:29:35 +0530

    add assignment one

--- 01_data_munging.js
+++ 01_data_munging.js
@@ -2,25 +2,41 @@ const fs = require('fs');
 const csv = require('fast-csv');
 
 
-const listOfTempSpread = [];
+const listOfRows = [];
 let count = 0;
-let innerCount =0;
+const tempSpreads = {};
 
 fs.createReadStream('./data/weather.dat')
   .pipe(csv.parse({ header: true }))
   .on('data', (entry) => {
-    //console.log(entry);
     count += 1;
+    let listOfStats = [];
     if (count > 2) {
-      let listOfStats = [];
       const word = entry[0].split(' ');
       listOfStats = word.filter((element) => element !== '');
-      console.log(listOfStats);
+      listOfRows.push(listOfStats);
     }
-   });
-
-// const fileData = fs.readFile('./data/weather.dat', "utf8", (err, data) => {
-//   console.log(data);
-// });
-
-// console.log('fileData:', fileData);
+  })
+  .on('end', () => {
+    listOfRows.forEach((statsRow) => {
+      let i = 1;
+      while (i < 3) {
+        if (statsRow[i].indexOf('*') !== 1) {
+          const filteredItem = statsRow[i].replace('*', '');
+          statsRow[i] = filteredItem;
+        }
+        tempSpreads[statsRow[0]] = statsRow[1] - statsRow[2];
+        i += 1;
+      }
+    });
+    const leastTempSpread = Math.min(...Object.values(tempSpreads));
+    let leastTempDay;
+    // eslint-disable-next-line no-restricted-syntax
+    for (let day in tempSpreads) {
+      if (tempSpreads[day] === leastTempSpread) {
+        leastTempDay = day;
+        break;
+      }
+    }
+    console.log(`The least temp spread is on the day ${leastTempDay} with temperature spread of ${leastTempSpread}`);
+  });
