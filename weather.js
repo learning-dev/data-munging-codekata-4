da983b1afa260c38ca8fb3861b984397f1bff88f
learning-dev <ayazm.140895@gmail.com>
2019-12-06 20:04:27 +0530

    add independent class for weather and football for each file

--- weather.js
+++ weather.js
@@ -1,17 +1,17 @@
 const File = require('./base/file.js');
 
-class SoccerFile extends File {
+class WeatherFile extends File {
   // eslint-disable-next-line no-useless-constructor
   constructor(filename, elementRow, firstIndex, secondIndex) {
     super(filename, elementRow, firstIndex, secondIndex);
     this.filename = filename;
   }
 
-  printMinDiffTeam() {
+  printMinTempSpread() {
     super.readInputFileProcess().then((element) => {
-      console.log(`The Smallest diff Team is ${element[0]} with difference of ${element[1]} goal.`);
+      console.log(`Minimum Temperature Spread is on day ${element[0]} with Temperature spread of ${element[1]}.`);
     });
   }
 }
 
-module.exports = SoccerFile;
+module.exports = WeatherFile;
